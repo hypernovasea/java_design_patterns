6f8b1e3cd9e74e13b8ad489641d38383929f2206
Artemis <fs031090@gmail.com>
2021-08-21 18:56:37 -0400

    refactoring

--- template_method/MysteryStory.java
+++ template_method/MysteryStory.java
@@ -1,21 +1,18 @@
 package template_method;
 
-/**
- * Created by artemis on 7/3/17.
- */
 public class MysteryStory extends Story {
 
-    void beginning(){
+    void beginning() {
         System.out.println("MysteryStory::Someone is murdered!");
     }
 
 
-    void conflict(){
+    void conflict() {
         System.out.println("MysteryStory::The police tries to find the killer!");
     }
 
 
-    void end(){
+    void end() {
         System.out.println("MysteryStory::The police find the killer just before he kills again!");
     }
 }
